218aa847241f8b7e0e9584c5d7b433b85bc9f231
Donald <donngraphics@gmail.com>
2024-12-06 21:55:05 -0600

    Add functionality for user to delete the grid, reset the grid, and create a grid of their choosing

--- script.js
+++ script.js
@@ -1,25 +1,96 @@
 const container = document.getElementById("container");
 const background = document.getElementById("body");
-
-// background.style["background-color"] = "red";
-//const divTest = document.createElement("div");
-//divTest.setAttribute("class", "divStyle");
-
-//container.appendChild(divTest);
+const newGrid = document.getElementsByClassName("divStyle")[0];
+const btn = document.getElementById("gridButton");
+const deleteBtn = document.getElementById("deleteButton");
+const resetBtn = document.getElementById("resetButton");
 
 
-for (i = 0; i < 256; i++) {
-    const divGrid = document.createElement("div");
-    divGrid.setAttribute("class", "divStyle");
-    container.appendChild(divGrid);
+function gridLoop(gridSize = 16) {
 
-    divGrid.addEventListener("mouseover", () => {
-        // divGrid.setAttribute("class", "hoverOver");
-        divGrid.style["background-color"] = "#ffdfbf";
+    let divGrid = document.querySelectorAll(".divStyle");
+    console.log(divGrid);
+    divGrid.forEach((divGrids) => {
+        divGrids.remove();
     });
 
+    let calcSize = 100 / gridSize;
+
+
+
+    for (i = 0; i < 256; i++) {
+
+        const divGrid = document.createElement("div");
+        divGrid.setAttribute("class", "divStyle");
+        divGrid.style["flex"] = "1 1 calc(" + calcSize + "% - 5px)";
+        container.appendChild(divGrid);
+
+        divGrid.addEventListener("mouseover", () => {
+            divGrid.style["background-color"] = "#ffdfbf";
+        });
+
+    }
 
 }
+
+gridLoop();
+
+resetBtn.addEventListener("click", () => {
+    let divGrid = document.querySelectorAll(".divStyle");
+    console.log(divGrid);
+    divGrid.forEach((divGrids) => {
+        divGrids.remove();
+    });
+
+    for (i = 0; i < 256; i++) {
+
+
+        const divGrid = document.createElement("div");
+        divGrid.setAttribute("class", "divStyle");
+        divGrid.style["flex"] = "1 1 calc(6.25% - 5px)";
+        container.appendChild(divGrid);
+
+        divGrid.addEventListener("mouseover", () => {
+            divGrid.style["background-color"] = "#ffdfbf";
+        });
+
+    }
+
+
+});
+
+
+deleteBtn.addEventListener("click", () => {
+    let divGrid = document.querySelectorAll(".divStyle");
+    console.log(divGrid);
+    divGrid.forEach((divGrids) => {
+        divGrids.remove();
+    });
+
+    //document.body.style.backgroundColor = "red";
+
+});
+
+
+
+
+btn.addEventListener("click", () => {
+
+    let gridPrompt = prompt("Grid size? e.g. 16 (16 pixels x16 pixels)", "");
+
+    if (gridPrompt > 100) {
+        alert("Sorry, no more than 100 pixels!");
+
+    } else if (isNaN(gridPrompt)) {
+        alert("Input must be a number (e.g. 48)");
+
+    } else {
+        gridLoop(gridPrompt);
+    }
+
+});
+
+
 
 
 
